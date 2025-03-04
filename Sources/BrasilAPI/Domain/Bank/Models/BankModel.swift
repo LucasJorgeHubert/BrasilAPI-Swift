143cd80e4f67d45c4f99998fbbf4c7409720e4e4
Lucas Jorge Hubert <lucasjhubert@gmail.com>
2025-03-04 15:38:41 -0300

    Feat: Setup completed and tested

---
+++ Sources/BrasilAPI/Domain/Bank/Models/BankModel.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public struct BankModel: Codable {
+    let ispb: String
+    let name: String?
+    let code: Int?
+    let fullName: String?
+}
--- Tests/BrasilAPITests/BrasilAPITests.swift
+++ Tests/BrasilAPITests/BrasilAPITests.swift
@@ -2,11 +2,16 @@ import XCTest
 @testable import BrasilAPI
 
 final class BrasilAPITests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    var useCase: GetBankListUseCase!
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    override func setUp() {
+        let repository = BankRepositoryImpl()
+        APIConfig.environment = .local
+        useCase = GetBankListUseCase(repository: repository)
+    }
+
+    func test_integration_fetchBanks() async throws {
+        let banks = try await useCase.execute()
+        XCTAssertFalse(banks.isEmpty)
     }
 }
